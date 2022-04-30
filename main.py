6ec93edb8637f1d413f30efc24bd3c2a103126a2
JoseLuria <jos02-log18@hotmail.com>
2022-04-29 20:55:39 -0500

    Nuevo codigo

--- main.py
+++ main.py
@@ -4,53 +4,67 @@ import fnmatch
 import csv
 from tokenize import Number
 
+from pkg_resources import to_filename
+
 files_names_list = []
 
 for file_name in os.listdir("test_files"):
     if fnmatch.fnmatch(file_name, "*.csv"):
         files_names_list.append(file_name)
 
-print(files_names_list)
-
-users_files_object = {
-
-}
-
 
 class FileQuiz:
-    def __init__(self, file_route: str) -> None:
-
-        self.__file_route = file_route
+    def __init__(self, file_routes: list[str]) -> list[str]:
+        
+        self.__file_routes = file_routes
+        self.list_all_student = []
 
     def getQuiz(self):
-        with open(f"test_files/{self.__file_route}", encoding='utf8') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
+        
+        for each_file in self.__file_routes:
+            
+            with open(f"test_files/{each_file}", encoding='utf8') as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+            
+                for user_row in csv_reader:
+                    user_row_data = {"name_complete": user_row["First Name"].lower() + " " + user_row["Last Name"].lower(), "score": int(
+                        user_row["Score"])}
 
-            quiz = []
+                    self.list_all_student.append(user_row_data)
 
-            for user_row in csv_reader:
-                user_row_data = {"firstname": user_row["First Name"], "lastname": user_row["Last Name"], "score": int(
-                    user_row["Score"])}
-
-                quiz.append(user_row_data)
-
-            return quiz
-
-
-def counter_test(parameter):
-    counter = 0
-    list_test = []
-
-    for number in range(len(parameter)):
-        new_list = FileQuiz(parameter[counter])
-        counter += 1
-
-        list_test.append(new_list.getQuiz())
-        # list_test.append("#################")
-
-    return list_test
+    # def getUserBestStudents(self, quantity_students: int) -> list[str]:
+            
 
 
-all_result = counter_test(files_names_list)
+def format_text_in_list(text: str):
+    formated_list = []
+    
+    if(" " in text):
+        formated_list = text.split(" ")
+    else:
+        formated_list = text.split("_")
+       
+    return formated_list 
 
-print(all_result)
+
+
+
+all_data = FileQuiz(files_names_list)
+all_data.getQuiz()
+# print(all_data.getUserBestStudents(4))
+
+# print(all_data_list)
+
+
+# asfasfasfafasfas
+# fsaasfasfafafasfasf
+# afassafasfsa
+
+
+# final_lits_to_reduce = [{}, {}, {}]
+
+# oredr_list = final_lits_to_reduce.(() => sore) 
+
+# oreder_list[0, quantity_students -1]
+
+# return final_list 
